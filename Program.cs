9c99f070ed19be9f62f076f1cca7d26afe703374
andreicastillo-cyber <andreicastillo200611@gmail.com>
2026-04-16 01:06:26 +0800

    product class and basic structure added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,33 @@
 using System;
 
+class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int RemainingStock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"{Id}. {Name} - ₱{Price} (Stock: {RemainingStock})");
+    }
+
+    public bool HasEnoughStock(int quantity)
+    {
+        return quantity <= RemainingStock;
+    }
+
+    public double GetItemTotal(int quantity)
+    {
+        return Price * quantity;
+    }
+
+    public void DeductStock(int quantity)
+    {
+        RemainingStock -= quantity;
+    }
+}
+
 class Program
 {
     static void Main()
